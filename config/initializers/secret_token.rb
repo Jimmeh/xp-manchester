7593e0c60cc8e39833e92bc0f4c0d620b03ad900
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-13 18:03:37 +0000

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XpManchester::Application.config.secret_token = 'cd1308939698a5fa0720baed189ece66badf71f3acb77707903510fde2a4350d894569dd03e85a78caccedaca59f899ba9c153a1c51dc0a63a23ab48015514ad'
