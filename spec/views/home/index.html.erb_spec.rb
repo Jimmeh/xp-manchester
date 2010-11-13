29adf6430a1f1c936e9ca1ba955dfd24aa10e927
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-13 21:30:39 +0000

    added link to mailing list to index page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -5,4 +5,11 @@ describe "home/index.html.erb" do
     render
     rendered.should have_selector("h1", :content => "XP Manchester")
   end
+  
+  it "should have a link to the mailing list" do
+    render
+    rendered.should have_selector("a", 
+      :content => "mailing list",
+      :href => "https://groups.google.com/group/xp-manchester?hl=en")
+  end
 end
