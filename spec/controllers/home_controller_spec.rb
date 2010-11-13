f19bc2f19364a6deb11aac6a7301098c54311e2c
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-13 18:26:07 +0000

    default route test added

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,15 @@ describe HomeController do
       get 'index'
       response.should be_success
     end
+    
+    it "should be default route" do
+      { :get => '/'}.should route_to(
+        :controller => "home",
+        :action => "index"
+      )
+    end
   end
+  
+  
 
 end
