14652ff0647a51c2a574b7b8e4120ebbd15999a7
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-14 18:38:09 +0000

    added link to madlab

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe "home/index.html.erb" do
+  it "should have link to madlab website" do
+    render
+    rendered.should have_selector("a",
+      :href => "http://www.madlab.org.uk",
+      :content => "MadLab, the Northern Quarter")
+  end
 end
