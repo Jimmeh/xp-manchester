6a4673c9be576efef9e3c506131dc0aae91fff69
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-14 16:48:40 +0000

    moved header stuff to layout

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe "home/index.html.erb" do
-  it "should have XP Manchester in a h1 tag" do
-    render
-    rendered.should have_selector("h1", :content => "XP Manchester")
-  end
-  
-  it "should have a link to the mailing list" do
-    render
-    rendered.should have_selector("a", 
-      :content => "mailing list",
-      :href => "https://groups.google.com/group/xp-manchester?hl=en")
-  end
 end
