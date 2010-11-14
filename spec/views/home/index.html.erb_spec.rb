041e45d3cc399a1e57ad35c66d0ae67065dfdbd3
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-14 18:41:00 +0000

    added link to c2.com xp wiki

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -7,4 +7,11 @@ describe "home/index.html.erb" do
       :href => "http://www.madlab.org.uk",
       :content => "MadLab, the Northern Quarter")
   end
+  
+  it "should have link to c2.com xp site" do
+    render
+    rendered.should have_selector("a",
+      :href => "http://c2.com/cgi/wiki?ExtremeProgramming",
+      :content => "see here")
+  end
 end
